0f18e4819c9a2bf8e84caf50d79aa97d431cdd3f
Ajay <amajaying@gmail.com>
2022-08-19 13:44:17 +0530

    codes

--- Assignment 3/Q3_sparse_matrix.c
+++ Assignment 3/Q3_sparse_matrix.c
@@ -2,7 +2,7 @@
 int main(){
     int n,m;
     printf("Enter size of 2D array: ");
-    scanf("%d%d", &m, &n);
+    scanf("%d%d", &n, &m);
     int arr[n][m];
     int zeroCount = 0;
     int oneCount = 0;
@@ -12,9 +12,26 @@ int main(){
             scanf("%d", &arr[i][j]);
             if(arr[i][j]==0)
                 zeroCount++;
-            else if(arr[i][j]==1)
-                oneCount++;
         }
+    }
+    printf("\n");
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Zero = %d, One = %d", zeroCount,oneCount);
+    int total = n*m;
+    printf("Total = %d", total);
+    float th = (((float)zeroCount/total)*100);
+    printf("Th = %f", th);
+    if(th<60 || th>74){
+        printf("Not Sparse!");
+    }
+    else{
+        printf("Sparse");
     }
 
 }
