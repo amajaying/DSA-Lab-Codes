8e17a5209024719ccd750942b7f6020aba48ae43
Ajay Khatri <amajaying@gmail.com>
2022-08-25 23:17:58 +0530

    codes

--- Assignment 3/Q3_sparse_matrix.c
+++ Assignment 3/Q3_sparse_matrix.c
@@ -1,23 +1,30 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-    int n,m;
-    printf("Enter size of 2D array: ");
-    scanf("%d%d", &m, &n);
-    int arr[n][m];
-    int zeroCount = 0;
-    int oneCount = 0;
+    int r,c;
+    printf("Enter the no of rows and columns: ");
+    scanf("%d %d",&r,&c);
+    int arr[r][c];
     printf("Enter the elements: ");
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            scanf("%d", &arr[i][j]);
-            if(arr[i][j]==0)
-                zeroCount++;
-            else if(arr[i][j]==1)
-                oneCount++;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
         }
     }
-    int x = 
-
-
+    int count=0;
+    int total=r*c;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            if(arr[i][j]==0){
+                count++;
+            }
+        }
+    }
+    float percentage = (float)count*100/total;
+    if(percentage<=60 || percentage>=75){
+        printf("Not sparse");
+    }else{
+        printf("Sparse");
+    }
+    return 0;
 
 }
