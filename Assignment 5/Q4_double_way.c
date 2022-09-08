c254f4e49e2e9c10c12fcc44e9c6a169b74eba4a
Ajay <amajaying@gmail.com>
2022-09-08 20:06:40 +0530

    codes

--- Assignment 5/Q4_double_way.c
+++ Assignment 5/Q4_double_way.c
@@ -8,3 +8,71 @@ struct node{
 
 struct node *head = NULL;
 
+void isEmpty() {
+    if(head == NULL){
+        printf("The linked list is EMPTY!\n");
+    }
+}
+
+void insertAtFront()
+{
+    struct node* temp;
+    temp = (struct node*)malloc(sizeof(struct node));
+    printf("\nEnter number to be inserted: ");
+    scanf("%d", &temp->data);
+    temp->prev = NULL;
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtEnd()
+{
+    struct node *temp, *trav;
+    temp = (struct node*)malloc(sizeof(struct node));
+    temp->prev = NULL;
+    temp->next = NULL;
+    printf("\nEnter number to be inserted: ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+    trav = head;
+
+    if (head == NULL) {
+ 
+        head = temp;
+    }
+    else {
+        while (trav->next != NULL)
+            trav = trav->next;
+        temp->prev = trav;
+        trav->next = temp;
+    }
+}
+
+void deleteFirst()
+{
+    struct node* temp;
+    if (head == NULL)
+        printf("\nList is empty\n");
+    else {
+        temp = head;
+        head = head->next;
+        if (head != NULL)
+            head->prev = NULL;
+        free(temp);
+    }
+}
+void deleteEnd()
+{
+    struct node* temp;
+    if (head == NULL)
+        printf("\nList is empty\n");
+    temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+    if (head->next == NULL)
+        head = NULL;
+    else {
+        temp->prev->next = NULL;
+        free(temp);
+    }
+}
